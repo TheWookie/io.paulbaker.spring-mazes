24ed643ca74c1a3886bd2d4315fd1e9cd1d792db
Paul Baker <paul.n.baker@utah.edu>
2016-08-21 03:41:20 -0600

    Init commit.

---
+++ src/main/java/io/paulbaker/springmazes/MazeController.java
@@ -0,0 +1,10 @@
+package io.paulbaker.springmazes;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by paulbaker on 8/21/16.
+ */
+@Controller
+public class MazeController {
+}
---
+++ src/main/java/io/paulbaker/springmazes/SpringMazesApplication.java
@@ -0,0 +1,12 @@
+package io.paulbaker.springmazes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMazesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMazesApplication.class, args);
+	}
+}
